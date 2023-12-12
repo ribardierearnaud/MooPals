36f8e70c6a48e702614d75865ee10798a9a0f01b
Arnaud Ribardière <arnaud.ribardiere@gmail.com>
2023-12-12 16:35:04 +0100

    Add EE

--- assets/script.js
+++ assets/script.js
@@ -128,3 +128,23 @@ function showDiscordMessage() {
     // Fermeture de la fenêtre modale
     document.getElementById('discord-modal').style.display = 'none';
   }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Track the number of clicks on the Special image
+    var specialImage = document.querySelector('.special-explanations img');
+    var clickCount = 0;
+    var maxClicks = 3; 
+  
+    // Function to handle image click
+    function handleImageClick() {
+      clickCount++;
+  
+      // Check if the desired number of clicks is reached
+      if (clickCount === maxClicks) {
+        specialImage.src = 'assets/lego.png';
+      }
+    }
+  
+    // Add click event listener to the Special image
+    specialImage.addEventListener('click', handleImageClick);
+  });
